1d25fa60b0ff332b43fdcfb10111ac90ca852823
vel_ana <76497435+Lireien@users.noreply.github.com>
2021-05-03 13:34:48 +0300

    tests

---
+++ division.test.js
@@ -0,0 +1,25 @@
+const division = require('./division');
+
+test('divide 10 / 2 to equal 5', () => {
+  expect(division(10, 2)).toBe(5);
+});
+
+test('divide 10 / 3 to equal 3.33', () => {
+  expect(division(10, 3)).toBe(3.3333333333333335);
+});
+
+test('divide true / 2 to equal 0.5', () => {
+  expect(division(true, 2)).toBe(0.5);
+});
+
+test('divide true / 0 to equal Infinity', () => {
+  expect(division(true, 0)).toBe(Infinity);
+});
+
+test('divide false / 0 to equal NaN', () => {
+  expect(division(false, 0)).toBe(NaN);
+});
+
+test('divide `6` / `2` to equal 3', () => {
+  expect(division(`6`, `2`)).toBe(3);
+});
