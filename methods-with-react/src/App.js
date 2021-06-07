666e110d391e328e142c015079eb110bc1fbc9b5
vel_ana <76497435+Lireien@users.noreply.github.com>
2021-06-07 18:46:20 +0300

    Methods become a lists, tests are not ready

--- methods-with-react/src/App.js
+++ methods-with-react/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import List from "./components/List";
 
+
+const headerText = `Let's now a little about methods in JS with React!`
 function App() {
+  const methods = Object.getOwnPropertyNames(Array.prototype);
+  const mutatingMethods = ['copyWithin', 'fill', 'map', 'push', 'pop', 'reverse', 'shift', 'unshift', 'sort', 'splice'];
+  const nonMutatingMethods = methods.filter(m => typeof Array.prototype[m]).filter(m => !mutatingMethods.includes(m));
   return (
-    <div className="App">
+    <div className="Methods">
       <header className="App-header">
+        <h1>Methods</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p className="Header-description">
+         {headerText}
+       </p>
+
       </header>
+      <section className="Methods-body">
+        <List list = {mutatingMethods} title = "Mutating"/>
+        <List list = {nonMutatingMethods} title = "Non mutating"/>
+
+      </section>
     </div>
   );
 }
--- methods-with-react/src/App.test.js
+++ methods-with-react/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/let's now a little about methods in js with react!/i);
+  expect(textElement).toBeInTheDocument();
 });
