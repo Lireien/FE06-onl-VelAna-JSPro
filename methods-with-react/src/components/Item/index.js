a27c472517f7276cbf166ded6d63951c9ccc4d0c
vel_ana <76497435+Lireien@users.noreply.github.com>
2021-06-30 16:14:33 +0300

    useCallback in action

--- methods-with-react/src/components/Item/index.js
+++ methods-with-react/src/components/Item/index.js
@@ -2,16 +2,19 @@ import {StyledButton, StyledLi} from "./StyledLi";
 import {LIST_TYPES} from "../../const";
 import {useDispatch} from "react-redux";
 import {changeType} from "../../store/list";
+import {useCallback} from "react";
 
 const Item = ({ item }) => {
   const dispatch = useDispatch();
+  const changeTypeToM = useCallback(()=> dispatch(changeType({name: item.name, type: LIST_TYPES.MUTATING})),[item.name]);
+    const changeTypeToN = useCallback(()=> dispatch(changeType({name: item.name, type: LIST_TYPES.NON_MUTATING})),[item.name]);
   return (
     <StyledLi data-tooltip={item.name}>
-      <StyledButton onClick={() => dispatch(changeType({name: item.name, type: LIST_TYPES.MUTATING}))}>
+      <StyledButton onClick={changeTypeToM}>
         ⇦
       </StyledButton>
       {` ${item.name} `}
-      <StyledButton onClick={() => dispatch(changeType({name: item.name, type: LIST_TYPES.NON_MUTATING}))}>
+      <StyledButton onClick={changeTypeToN}>
         ⇨
       </StyledButton>
     </StyledLi>
